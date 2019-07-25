419534831458e5f666e71310375c7cab690fc904
picarica <dohaje@azet.sk>
2019-07-25 02:03:56 +0200

    added Readme and made some nice changes to the script

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+disk_Backup : serves purpose of backing up whole drive with all partitions 
+
+megasync : is just tool that refixes my megasync icon
+
+synchronyze_exclude : list that tells zal_jj_rsync which folder to exclude
+
+zal_jj_rsync : backup made by Rsync
--- disk_backup.sh
+++ disk_backup.sh
@@ -1,3 +1,7 @@
  #!/bin/bash
 
-dd if=/dev/sdb | pv -s 240057409536 |  dd of=/run/media/picarica/565324e2-5f91-486b-91c6-6ad927484668/ssd-backup.img && echo complete            
+B_DRIVE="/dev/sdb"
+DISK_SIZE="240057409536"
+O_DRIVE="=/run/media/picarica/565324e2-5f91-486b-91c6-6ad927484668/"
+
+dd if=${B_DRIVE} | pv -s ${DISK_SIZE} |  dd of=${O_DRIVE}/ssd-backup.img && echo complete            
